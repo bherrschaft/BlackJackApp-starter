d2e03c8ccebadedc48e84bc19bd30a8af8acd734
bherrschaft <bherrschaft92994@gmail.com>
2024-08-14 14:32:42 -0400

    Update DeckActions.java

--- DeckActions.java
+++ DeckActions.java
@@ -1,7 +1,18 @@
 public interface DeckActions {
-    public void shuffle() ;
+    // Method to shuffle the deck of cards
+    public void shuffle(); // Shuffles the cards in the deck
 
-    public Card dealNextCard();
+    // Method to deal the next card from the deck
+    public Card dealNextCard(); // Deals the next card from the top of the deck
 
-    public void printDeck(int numToPrint) ;
+    // Method to print a specified number of cards from the deck
+    public void printDeck(int numToPrint); // Prints a specified number of cards from the deck
 }
+
+/*
+ * Summary:
+ * The DeckActions interface defines the essential actions that can be performed on a deck of cards. 
+ * It includes methods to shuffle the deck, deal the next card, and print a specified number of cards. 
+ * Any class that implements this interface must provide concrete implementations of these methods. 
+ * This interface is particularly useful in card game applications where decks need to be manipulated in a standard way.
+ */
